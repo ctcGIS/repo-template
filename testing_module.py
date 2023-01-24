1186133ccba05b699f57e735b9a34d0fc714751c
sbrim-CTC <sbrim@ctcnet.us>
2023-01-24 13:43:12 -0500

    added and Universe to the hello world statement

--- testing_module.py
+++ testing_module.py
@@ -1,6 +1,6 @@
 def sayHello(name=None):
     # Function to print Hello World with the name variable
     if name == None:
-        print("Hello World")
+        print("Hello World and Universe")
     else:
         print("Hello World " + str(name))
